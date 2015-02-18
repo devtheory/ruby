e5b831a52764091900f1487dc64824f9af4c48e2
Jose Sanchez <jparkers0012@gmail.com>
2015-02-18 14:43:38 +0000

    Accessing, getting info from, adding items to and removing items from an array

--- Arrays/basics.rb
+++ Arrays/basics.rb
@@ -6,9 +6,37 @@ array = Array.new(3, false) # Creates [false, false, false]
 array = Array.new(3) { Hash.new } # Creates [{}, {}, {}]
 table = Array.new(3) { Array.new(3) } # Createa [[nil,nil,nil], [nil,nil,nil], [nil,nil,nil]]
 
-# Accessing and array
+# Accessing an array
 array = ["first", "second", "third"]
 array[0] # returns "first"
 array[-1] # returns "third"
 array[0, 2] # returns ["first", "second"] where first is start, last is length
 array[0..-2] # returns ["first", "second"]
+array.fetch(3, "not found") # returns the error if the index is not found
+array.first # returns first value in the array
+array.last # returns last value in the array
+array.take(2) # returns first two elements of array
+array.drop(1) # returns array without the first element
+
+# Getting information from an array
+array.length || array.size || array.count # returns number of elements in array
+array.empty? # returns true if array is empty
+array.includes?('four') # returns false
+
+# Adding items to an array
+array.push('four') # appends four to the end of the array
+array << 'five' # appends five to the end of the array
+array.unshift(0) # prepends zero to the beginning of the array
+array.insert(2, 'two and a half') # inserts second element at index and shifts the rest down
+array.insert(2, "two and a half, two and a quarter") # inserts elements at index
+
+# Removing items from an array
+last = array.pop # REMOVES and RETURNS last element in array. Mutates array.
+first = array.shift # REMOVES and RETURNS first element in array. Mutates array.
+array.delete_at(2) # REMOVES and RETURNS element at index 2
+array.delete(2) # REMOVES every instance of the argument in the array
+array.compact # REMOVES all nil values. Does NOT mutate array
+array.compact! # REMOVES all nil values. Mutates array.
+array.uniq # REMOVES duplicated values. Does NOT mutate array
+array.uniq! # REMOVES duplicated values. Mutates array.
+
