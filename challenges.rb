bad42e57db107ed58988ce2a8d1c21e5029ddd41
Jose Sanchez <jparkers0012@gmail.com>
2015-02-23 04:44:59 +0000

    letter changer challenge

--- challenges.rb
+++ challenges.rb
@@ -24,3 +24,18 @@ def longest_word(string)
     end
     longest
 end
+
+# Replace letters in String with next letter in alphabet. Capitalize vowels
+def letter_changes(str)
+    alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z] # creates an array with all letters in the alphabet
+    
+    element_array = str.downcase.split('') #normalizes the string and then splits at every element into array
+    new_array = [] # holder
+    
+    element_array.each do |el|
+        # if the element is not a letter, add it. else if its z, add A. else, get the index of the element and add the next letter in the alphabet
+        new_array << (!alphabet.include?(el) ? el : (el == 'z' ? 'A' : alphabet[alphabet.index(el) + 1]))
+    end
+    # capitalize all vowels and return the new string
+    new_array.join.gsub!(/[aeiou]/, 'a' => 'A', 'e' => 'E', 'i' => 'I', 'o' => 'O', 'u' => 'U')
+end
