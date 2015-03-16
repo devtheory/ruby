1c7e60f9fefd948b35004eebca21aa79472c45bc
Jose Sanchez <jparkers0012@gmail.com>
2015-03-16 01:56:47 +0000

    gsub

--- basics.rb
+++ basics.rb
@@ -9,3 +9,8 @@ end
 array.each do |el|
     prints el, " "
 end
+
+# gsub regex
+string = "th#e br05wn f*ox"
+no_special_characters = string.gsub(/[^ 0-9a-zA-Z]/, '') #^ performs negation. Replaces every character not in the regex with nothing. Does not mutate
+only_special_characters = string.gsub!(/[ 0-9a-zA-Z]/, '') # replaces every character in the regex with nothing. mutates string!
